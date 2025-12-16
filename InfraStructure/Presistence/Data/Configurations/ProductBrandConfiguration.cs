33dd8a0f612f9407d0ed58616e6f7d53128f3504
MostafaSaad-710 <mustafasaad2274@gmail.com>
2025-12-16 20:50:07 +0200

    Add project files.

---
+++ InfraStructure/Presistence/Data/Configurations/ProductBrandConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domaine.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data.Configurations
+{
+    public class ProductBrandConfiguration : IEntityTypeConfiguration<ProductBrand>
+    {
+        public void Configure(EntityTypeBuilder<ProductBrand> builder)
+        {
+            builder.Property(P => P.Name).HasColumnType("varchar").HasMaxLength(256);
+
+        }
+    }
+}
---
+++ InfraStructure/Presistence/Data/Configurations/ProductTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domaine.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data.Configurations
+{
+    public class ProductTypeConfiguration : IEntityTypeConfiguration<ProductType>
+    {
+        public void Configure(EntityTypeBuilder<ProductType> builder)
+        {
+            builder.Property(P => P.Name).HasColumnType("varchar").HasMaxLength(256);
+        }
+    }
+}
